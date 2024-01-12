2d5ee4ae8d90267e95f252e2544efc55439a4771
Quive87 <6e09.ayushmalik@gmail.com>
2024-01-12 16:31:17 +0530

    new

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,23 +5,36 @@ import './Navigation.css'
 const Navigation = () => {
   const location = useLocation();
   const path = location.pathname;
-
   const getPageName = () => {
     switch (path) {
       case '/docx':
         return 'Docx';
       case '/legal':
         return 'LegalAI';
-      case '/modin1':
-        return 'News';
-      default:
+      case '/':
         return 'Home';
-    }
-  };
+      default:
+        return 'News';
+    }};
+
+  const getBtnType = () => {
+      switch (path) {
+        case '/docx':
+          return '<- Home';
+        case '/legal':
+          return '<-Home';
+        case '/modin1':
+          return 'Summary';
+        case '/testsample':
+          return 'Summary';
+        default:
+          return 'Menu';
+    }};
+
   return (
-<nav className="fixed top-0 left-0 right-0 flex items-center justify-between px-4 py-2 bg-transparent shadow-md bg-opacity-30 text-gray-300 border-b border-gray-700 z-10">
+<nav className="fixed top-0 left-0 right-0 flex items-center justify-between px-4 py-2 bg-transparent shadow-md bg-opacity-30 text-gray-300 border-b border-gray-700 z-20">
   <div className="flex items-center">
-    <img src="https://quive87.github.io/gg/pro/adawhite.png" alt="Logo" className="w-auto h-10 md:w-25 md:h-14 mr-4" />
+    <img src="https://quive87.github.io/gg/pro/adawhite.png" alt="Logo" className="w-auto h-10 md:w-25 md:h-12 mr-4" />
     <span className="font-bold text-lg md:text-xl">{getPageName()}</span>
   </div>
   <ul className="hidden lg:flex space-x-4">
@@ -32,7 +45,7 @@ const Navigation = () => {
   <button
     className="bg-gray-800 text-gray-100 hover:bg-gray-700 px-3 py-1 rounded-full md:px-4 md:py-2"
   >
-    Search
+    {getBtnType()}
   </button>
 </nav>
 
--- src/pages/Docx.js
+++ src/pages/Docx.js
@@ -1,18 +1,14 @@
 import Navigation from "../components/Navigation";
 
 const Docx = () => {
-    return (
-        <div>
-            <Navigation />
-            <div class="dark:bg-gray-800 p-4 rounded-md shadow-md">
-            <iframe 
-  src="https://www.myscheme.gov.in/find-scheme"  
-  class="rounded-md overflow-hidden w-full h-96 mt-[-50px]"
-></iframe>
-</div>
 
-        </div>
-    );
-}
+  return (
+    <div>
+      <p>Daksh ne nai diya bhai...</p>
+      {/* You can include additional content here if needed */}
+    </div>
+  );
+};
+
 
 export default Docx;
